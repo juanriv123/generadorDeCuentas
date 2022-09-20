734150996ec3966ce828a4ce8875daa42f0683f5
juanriv123 <riverosjuan828@gmail.com>
2022-09-20 15:06:54 -0300

    maquetado 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -50,6 +51,7 @@ import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
     MdbTooltipModule,
     MdbValidationModule,
     BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/cuentas.service.ts
+++ src/app/services/cuentas.service.ts
@@ -7,15 +7,65 @@ import { Injectable } from '@angular/core';
 })
 export class CuentasService {
   
-  constructor() { }
+   nameNew:string = "";
+   lastnameNew:string = "";
+   priceNew!: any;
+   emailNew:string = ""; 
+   dateNew:any | undefined ;
 
-  // nombre:string = "";
-  // tiempo:any =;
-  // persona:any = [{
-  //   this.nombre:,
-  //   this.tiempo:
-  // }]
-  // cuentaNueva:string =""; 
+   name:string[] =[];
+   lastname:string[] =[];
+   price:any[] =[];
+   email:string[] =[]; 
+   date:any[] =[];
 
-  // }
+   numero:number = 0 ;  
+
+   data: any[] = [];
+
+//    data: any[] = [{
+//     name: '',
+//     price: 0,
+
+//    },
+// ]
+
+   agregarUser(){
+    this.name.push(this.nameNew);
+    this.lastname.push(this.lastnameNew);
+    this.price.push(this.priceNew);
+    this.email.push(this.emailNew);
+    this.date.push(this.dateNew);
+
+   this.numero = parseInt(this.dateNew)
+
+    this.data.push({
+      name: this.nameNew,
+      fechaInicio: this.dateNew,
+     }) 
+
+    this.nameNew = "";
+    this.lastnameNew ="";
+    this.priceNew = "" ;
+    this.emailNew ="";
+    this.dateNew ="";
+
+    
+   }
+
+   borrarUser(index: number){
+    this.name.splice(index, 1);
+    this.lastname.splice(index, 1);  
+    this.price.splice(index, 1);
+    this.email.splice(index, 1);
+    this.date.splice(index, 1);
+
+    this.data.splice(index, 1);
+   }
+
+   borrarTodo(){
+      for (let index = 0; index < 10000; index++) {
+         this.data.pop()
+      }
+   }
 }
