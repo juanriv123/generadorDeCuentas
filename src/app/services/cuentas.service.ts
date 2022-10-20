04dd27810110452543cd80ea1a416353206dfe0e
juanriv123 <riverosjuan828@gmail.com>
2022-10-19 22:54:30 -0300

    maquetado-2

--- src/app/services/cuentas.service.ts
+++ src/app/services/cuentas.service.ts
@@ -1,71 +1,70 @@
 // import { jitOnlyGuardedExpression } from '@angular/compiler/src/render3/util';
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
+
+
 // import { time } from 'console';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CuentasService {
   
    nameNew:string = "";
    lastnameNew:string = "";
-   priceNew!: any;
+   priceNew: any = 0;
    emailNew:string = ""; 
-   dateNew:any | undefined ;
-
-   name:string[] =[];
-   lastname:string[] =[];
-   price:any[] =[];
-   email:string[] =[]; 
-   date:any[] =[];
-
-   numero:number = 0 ;  
-
+   dateNew:any = new Date();
+   vencimientoNew:string ="";
    data: any[] = [];
 
-//    data: any[] = [{
-//     name: '',
-//     price: 0,
-
-//    },
-// ]
-
-   agregarUser(){
-    this.name.push(this.nameNew);
-    this.lastname.push(this.lastnameNew);
-    this.price.push(this.priceNew);
-    this.email.push(this.emailNew);
-    this.date.push(this.dateNew);
-
-   this.numero = parseInt(this.dateNew)
-
-    this.data.push({
+   constructor(private afs: AngularFirestore) {}
+   
+   userId = 'test-id'; // TODO traer id del user
+   collection = this.afs.collection(`usuarios/${this.userId}/suscripciones`)
+   collectionData: any = this.collection.snapshotChanges().pipe(map(arr => arr.map(snap => {
+    const data: any = snap.payload.doc.data();
+    const id = snap.payload.doc.id;
+    return {...data, id};
+   })))
+   agregarUser(){ 
+    const newData = {
       name: this.nameNew,
+      lastname: this.lastnameNew,
+      price: this.priceNew,
+      email: this.emailNew,
+      date: this.dateNew,
       fechaInicio: this.dateNew,
-     }) 
+      
+     };
+    this.data.push(newData) 
 
-    this.nameNew = "";
-    this.lastnameNew ="";
-    this.priceNew = "" ;
-    this.emailNew ="";
-    this.dateNew ="";
+     this.collection.add(newData).then(() => console.log('success') )
+     .catch(err => console.log(err) )
+
+     this.nameNew = "";
+     this.lastnameNew ="";
+     this.priceNew = 0;
+     this.emailNew ="";
+     this.dateNew ="";
 
     
    }
 
    borrarUser(index: number){
-    this.name.splice(index, 1);
-    this.lastname.splice(index, 1);  
-    this.price.splice(index, 1);
-    this.email.splice(index, 1);
-    this.date.splice(index, 1);
-
     this.data.splice(index, 1);
    }
 
    borrarTodo(){
-      for (let index = 0; index < 10000; index++) {
-         this.data.pop()
-      }
+      this.data = [];
    }
+
+  vencimientos(index: number){
+   this.data[index].fechaFinal = this.vencimientoNew;
+ this.vencimientoNew ="";
+  }
+  
+ 
 }
